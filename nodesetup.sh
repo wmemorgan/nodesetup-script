dd2a9cd53329033e342497ca8db9bfa22572059a
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-05 15:45:19 +0800

    Fix database connector install
    Add environment variable setup script

--- nodesetup.sh
+++ nodesetup.sh
@@ -7,6 +7,8 @@ echo "Do you want to enable logging? (y/N):"
 read installmorgan
 echo "Do you want to add a database? (y/N):"
 read installknex
+echo "Do you want to use environment variables? (y/N):"
+read installenv
 echo "Will you be deploying API to Netlify? (y/N):"
 read installnetlify
 
@@ -20,7 +22,6 @@ else
 fi
 
 if [ "$installmorgan" = 'y' ]; then
-  echo
   echo "Installing Morgan logging module"
   yarn add morgan
 fi
@@ -28,11 +29,23 @@ fi
 if [ "$installknex" = 'y' ]; then
   echo "Which database will you use (sqlite3/pg/both): "
   read databasetype
-  echo "Installing knex and database connector $databasetype"
-  yarn add knex $databasetype &&
+
+  if [ "$databasetype" = 'both']; then
+    echo "Installing knex and database connector $databasetype"
+    yarn add knex sqlite3 pg
+  else
+    yarn add knex $databasetype
+  fi
+
+  echo "Creating default data directory and configuration file"
   mkdir data && touch data/dbConfig.js &&
   mkdir data/models && touch data/models/index.js &&
   yarn knex init
+fi
+
+if [ "$installenv" = 'y' ]; then
+  echo "Setting up project to use environment variables"
+  yarn add dotenv && touch .env
 fi
 
 if [ "$installnetlify" = 'y' ]; then
